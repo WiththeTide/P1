cc08da8a6f64d68235fef93dcc4fb10fd146a799
jky5864 <jinkaiyu526591531@gmail.com>
2023-03-14 14:46:55 +0800

    change

--- Assets/Script/CalculateScore.cs
+++ Assets/Script/CalculateScore.cs
@@ -3,17 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 using player;
 using FoodSystem;
+using TMPro;
 
 public class CalculateScore : MonoBehaviour
 {
+    public TMP_Text score;
+    public int scoreNum;
+    private bool playerOn;
     // Start is called before the first frame update
+    private void Start()
+    {
+        scoreNum = 0;
+    }
     private void Update()
     {
-        
+        calculate();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Debug.Log("enterTheBin");
+        playerOn = true;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        //Debug.Log("ExitFridge");
+        playerOn = false;
     }
 
     private void calculate()
     {
-    
+        if (playerOn && Input.GetKeyDown(KeyCode.Space))
+        {
+            if (Player.pocket.Equals(Foodgeneration.dishnumber1.text))
+            {
+                scoreNum += 5;
+                score.text = scoreNum.ToString();
+            }
+        }
     }
 }
